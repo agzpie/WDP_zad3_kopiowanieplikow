86c6ef7924cf476df98077f437a90cc715153042
agzpie <agata.pietralik@student.wat.edu.pl>
2018-01-08 22:30:18 +0100

    finished and working code overwriting or creating a new file with text from a different one

--- main.c
+++ main.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-    int j = 0;
-
     if (argc != 3) {
-        printf("");
-        return 0;
+        printf("Wrong arguments");
+        return -1;
     }
 
-
-    FILE *fp = fopen(*argv, "r");
-    if (fp != NULL) {
-        if (i==2) {
-            FILE *file = fopen(*argv, "r+");
-            fprintf(file, "%d", 1); // regardless of what's in the file, i want to clear it and put 1 in there
-            // legacy code somewhere else in the code base. can't change.
-            rewind(file);
-            fprintf(file, "%d", 2);
-            fflush(file);
-        }
-
-        /*
-         int c;
-        int filechars = 0;
-        int filelines = 1;
-        while ((c = fgetc(fp)) != EOF) {
-            char cc = (char) c;
-            if (cc == '\n'){
-                filelines++;
-            }
-
-            filechars++;
-            j++;
-         }   */
-
-        printf("%s: %d chars, %d lines\n", *argv++, filechars, filelines);
-        fclose(fp);
+    FILE *source = fopen(*++argv, "r");
+    if (source == NULL) {
+        printf("Source file is invalid");
+        return -1;
     }
+
+    FILE *destination = fopen(*++argv, "w+");
+    if (destination == NULL) {
+        printf("Destination file is invalid");
+        return -1;
+    }
+
+    int c;
+    while( ( c = fgetc(source) ) != EOF )
+        fputc(c, destination);
+
+    //fputc(EOF, destination);
+
+    fclose(destination);
+    fclose(source);
 
     return 0;
 }
